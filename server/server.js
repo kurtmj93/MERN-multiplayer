26a42f1abc87bc241e38d2d32c102f69028fe461
Kurt Jensen <Kurtmj93@gmail.com>
2023-04-20 21:22:03 -0400

    clean up package and server - cors library redundant to socketio

--- server/server.js
+++ server/server.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
-port = process.env.PORT || 3001
+port = process.env.PORT || 3001;
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: `http://localhost:3000`,
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
+    origin: `http://localhost:3000` // this is the client origin
   }
 });
 
