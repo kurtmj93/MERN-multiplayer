1e955a930d610a288e8cd7003ae974a25eddb8ab
Kurt Jensen <Kurtmj93@gmail.com>
2023-04-20 21:13:24 -0400

    web socket connected!

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { io } from "socket.io-client";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +15,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const socket = io(`ws://localhost:3001`);
+
+socket.on("connect", () => {
+  console.log('you are connected');
+})
--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 port = process.env.PORT || 3001
-app.listen(port, () => {
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: `http://localhost:3000`,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
+io.on('connect', (socket) => {
+  console.log('user connected', socket.id);
+})
+
+
+httpServer.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
