215e599ac9704d7ae3c5b07f294319b86e193312
Kurt Jensen <Kurtmj93@gmail.com>
2023-05-01 19:33:48 -0400

    edit for heroku router issue

--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ const app = express();
 
 if (process.env.NODE_ENV === 'production') { 
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 };
 
 // need this for deploy to heroku - react-router wasn't working
